6dcd4b4747cf2bcb662645748dc403ba848755ef
weiguo.kong <weiguo.kong@qunar.com>
2018-04-04 18:25:22 +0800

    优化

--- bin/nomi.js
+++ bin/nomi.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 'use strict';
-const cli = require('../lib/cli'),
-    cmd = process.argv[2];
+const cli = require('../lib/cli');
+const cmd = process.argv[2]; // nomi cmd
 
-cli.run(cmd);
+cli.run(cmd); // cli 入口文件
