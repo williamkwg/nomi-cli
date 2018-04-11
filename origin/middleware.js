4ba41c2452cc3c78b3a3938ed2fd72ef6b4dc094
weiguo.kong <weiguo.kong@qunar.com>
2018-04-11 14:25:36 +0800

    新增 nomi bulid 命令

--- origin/middleware.js
+++ origin/middleware.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "global": [
+  "global": [ // 全局中间件：每一个 请求都会流经
      {
-      "name": "middlewareA",
+      "name": "middlewareA", //中间件名字
       "enable": true,  // 中间件启用与否
       "match": '*', // 符合正则的request 会流经该中间件  
       "options": {}
     }
   ],
-  "local": []
-}
+  "local": [] // 业务中间件
+};
